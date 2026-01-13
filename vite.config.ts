b1c7e4491bf65ba180d1016e2c8c7ece8de0abdb
luizspinola1404 <lspinolagoncalves@gmail.com>
2026-01-13 13:56:52 -0300

    Remove comment from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuração básica para Vite + React
 export default defineConfig({
   plugins: [react()],
   build: {
